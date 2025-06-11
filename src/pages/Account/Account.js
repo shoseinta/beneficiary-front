1629ce23f825f748f3ca9020d2403731d31db593
shoseinta <htaheri.76@gmail.com>
2025-06-11 17:21:18 +0330

    account1 implemented

--- src/pages/Account/Account.js
+++ src/pages/Account/Account.js
@@ -1,8 +1,40 @@
-import NavigationBar from "../../components/navigationBar/NavigationBar"
+import NavigationBar from "../../components/navigationBar/NavigationBar";
+import Header from "../../components/header/Header";
+import Account1 from "./components/Account1";
+import { useState,useEffect } from "react";
+
 function Account() {
+    const [step, setStep] = useState(1);
+    const [accountData, setAccountData] = useState(null);
+    
+
+     // Function to load account data
+    const loadAccountData = async () => {
+            try {
+                const response = await fetch(`http://localhost:8000/beneficiary-platform/beneficiary/${localStorage.getItem('user_id')}/information/`,
+                    {
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Token ${localStorage.getItem('access_token')}`,
+                        },
+                    }
+                );
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setAccountData(data);
+                // Process the data as needed
+            }catch (error) {
+                console.error('Error fetching account data:', error);
+            }
+        }
+    useEffect(() => {
+        loadAccountData();
+    }, []);
     return(
         <>
-            <NavigationBar selected={4}/>
+        {step === 1 && <Account1 accountData={accountData} setAccountData={setAccountData} setStep={setStep}/>}
         </>
     )
 }
---
+++ src/pages/Account/components/Account1.js
@@ -0,0 +1,223 @@
+
+import { useState,useEffect } from "react";
+import Header  from "../../../components/header/Header";
+import NavigationBar from "../../../components/navigationBar/NavigationBar";
+import './Account1.css';
+
+
+function Account1({accountData, setAccountData, setStep}) {
+    const [submit, setSubmit] = useState(false)
+    const [validation, setValidation] = useState({
+  phone_number: true,
+  email: true,
+});
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (validation.phone_number && validation.email){
+        try {
+          const response = await fetch(`http://localhost:8000/beneficiary-platform/beneficiary/${localStorage.getItem('user_id')}/update-user-register/`, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Token ${localStorage.getItem('access_token')}`,
+            },
+            body: JSON.stringify({email:accountData.email,phone_number:accountData.phone_number}),
+          });
+          if (!response.ok) {  // Check for HTTP errors (4xx/5xx)
+            const errorData = await response.json();
+            throw new Error(errorData.detail || 'Login failed');
+          }
+          const result = await response.json();
+          console.log(result)
+          
+          setSubmit(true);
+    
+        // Reset after 5 seconds
+        setTimeout(() => setSubmit(false), 5000);
+
+        } catch (err) {
+          console.log(err)
+        }
+    } else {
+        return
+    }
+    
+      };
+
+
+const [blur, setBlur] = useState(
+    {
+        phone_number:true,
+        email: true,
+    }
+)
+
+const handleEmailChange = (e) => {
+  setBlur(pre => ({...pre,email:false}))
+  const value = e.target.value;
+  if (!value){
+    setAccountData(prev => ({...prev, email: null}))
+  } else{
+    setAccountData(prev => ({...prev, email: value}));
+  }
+  
+  
+  // Email validation regex
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isValidEmail = value === '' || emailRegex.test(value); // Allow empty or valid email
+  
+  setValidation(prev => ({
+    ...prev,
+    email: isValidEmail
+  }));
+};
+
+const handlePhoneChange = (e) => {
+  setBlur(pre => ({...pre, phone_number:false}))
+  const value = e.target.value;
+  if (!value){
+    setAccountData(prev => ({...prev,phone_number: null}))
+  }else {
+    setAccountData(prev => ({...prev, phone_number: value}));
+  }
+  
+  
+  const phoneRegex = /^(۰۹|09)[0-9۰-۹]{9}$/;
+  const isValidPhone = value === '' || phoneRegex.test(value);
+  
+  setValidation(prev => ({
+    ...prev,
+    phone_number: isValidPhone
+  }));
+};
+    
+    useEffect(()=>{
+            document.documentElement.classList.add('account-container1-html')
+            document.body.classList.add('account-container1-body')
+    
+            return ()=>{
+                document.documentElement.classList.remove('account-container1-html')
+                document.body.classList.remove('account-container1-body')
+            }
+        },[])
+    return(
+        <div className="account-container1">
+            
+            <Header />
+            <main className="main">
+                <section>
+                    <h1>
+                    با انتخاب هر یک از موارد زیر، می‌توانید با تکمیل بخش‌های خالی اقدام به اشتراک اطلاعات خود با خیریه کنید.
+                    </h1>
+                </section>
+
+                <nav className="nav-up">
+                    <ul className="nav-list-up">
+                    <li  onClick={() => setStep(1)} className="nav-item-up" id="active-nav-up">
+                        <a>اطلاعات حساب کاربری</a>
+                    </li>
+                    <li onClick={() => setStep(2)} className="nav-item-up">
+                        <a>اطلاعات شخصی کاربر</a>
+                    </li>
+                    <li onClick={setStep(3)} className="nav-item-up">
+                        <a>اطلاعات آدرس کاربر</a>
+                    </li>
+                    <li onClick={setStep(4)} className="nav-item-up">
+                        <a>اطلاعات تکمیلی کاربر</a>
+                    </li>
+                    </ul>
+                </nav>
+
+                <form  className="account-form1">
+                    <div>
+                    <label htmlFor="account-support"> تحت پوشش خیریه: </label>
+                    <input type="text" id="account-support" readOnly value={accountData?.beneficiary_user_information?"هستید":"نیستید" || ""} />
+                    </div>
+
+                    <div>
+                    <label htmlFor="account-id"> کد مددجویی: </label>
+                    <input type="text" id="account-id" readOnly value={accountData?.beneficiary_id || ""} />
+                    </div>
+
+                    <div>
+                    <label htmlFor="account-password"> رمز عبور: </label>
+                    <input type="password" id="account-password" readOnly value={accountData?.password || ""} />
+                    </div>
+
+                    <div>
+                    <label htmlFor="account-phone-number"> شماره تلفن همراه: </label>
+                    <input
+                        type="tel"
+                        id="account-phone-number"
+                        pattern="^(۰۹|09)[0-9۰-۹]{9}$"
+                        maxLength="11"
+                        inputMode="numeric"
+                        placeholder="مثال: ۰۹۱۲۰۱۲۳۴۵۶"
+                        value={accountData?.phone_number || ""}
+                        onChange={handlePhoneChange}
+                        onBlur={() => setBlur(pre => ({...pre,phone_number:true}))}
+                        className={!validation.phone_number && blur.phone_number ? "invalid-input" : ""}
+                        
+                    />
+                    {!validation.phone_number && blur.phone_number && (
+                        <div className="error-message"> شماره همراه وارد شده صحیح نیست</div>
+                    )}
+                    </div>
+
+                    <div>
+                    <label htmlFor="account-email"> آدرس ایمیل: </label>
+                    <input 
+                    type="email" 
+                    id="account-email" 
+                    value={accountData?.email || ""} 
+                    onChange={handleEmailChange} 
+                    onBlur={() => setBlur(pre => ({...pre,email:true}))}
+                    className={!validation.email && blur.email ? "invalid-input" : ""}
+                    />
+                    {!validation.email && blur.email && (
+                        <div className="error-message">فرمت ایمیل وارد شده صحیح نیست</div>
+                    )}
+                    </div>
+
+                    <div id="account-submit1">
+                    {
+                        submit ?
+                    <div>
+                        <svg
+                        width="16"
+                        height="14"
+                        viewBox="0 0 16 14"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <path d="M14.7373 0L4.94741 11.1961L1.22585 6.93997L0 8.34191L4.94741 14L15.9631 1.40194L14.7373 0Z" />
+                        </svg>
+                        تغییرات با موفقیت اعمال شد
+                    </div>
+                    :
+                    <div style={{visibility:"hidden"}}>
+                        <svg
+                        width="16"
+                        height="14"
+                        viewBox="0 0 16 14"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                        >
+                        <path d="M14.7373 0L4.94741 11.1961L1.22585 6.93997L0 8.34191L4.94741 14L15.9631 1.40194L14.7373 0Z" />
+                        </svg>
+                        تغییرات با موفقیت اعمال شد
+                    </div>
+                    }
+                    
+                    <input type="submit" value="تأیید" onClick={handleSubmit} />
+                    </div>
+                </form>
+                </main>
+
+                <NavigationBar selected={4}/>
+        </div>
+    )
+}
+
+export default Account1;
